a618996d037c05627c84ff933067fc6375754af1
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2022-08-22 18:11:32 +0300

    Introduce a separate package for decoders

--- src/lib/gossip_net_fuzz/decode_interface.ml
+++ src/lib/gossip_net_fuzz/decode_interface.ml
@@ -7,7 +7,7 @@ let decode msg ~bin_prot =
   Bigstring.read_bin_prot b bin_prot.Bin_prot.Type_class.reader
   |> is_ok
 
-let decode_gossip_message msg = decode msg ~bin_prot:Message.Latest.T.bin_msg
+(* let decode_gossip_message msg = decode msg ~bin_prot:Message.Latest.T.bin_msg *)
 
 let decode_external_transition msg =
   decode msg ~bin_prot:External_transition.Raw.Stable.V1.bin_t
@@ -18,8 +18,12 @@ let decode_snark_pool_diff msg =
 let decode_tx_pool_diff msg =
   decode msg ~bin_prot:Transaction_pool.Diff_versioned.Stable.V1.bin_t
 
+(* let foo = *)
+(*   printf () "=== importand message from OCaml" *)
+
 let () =
-  Callback.register "decode_gossip_message" decode_gossip_message;
+  (* Callback.register "decode_gossip_message" decode_gossip_message; *)
+  (* Callback.register "foo" foo; *)
   Callback.register "decode_external_transition" decode_external_transition;
   Callback.register "decode_snark_pool_diff" decode_snark_pool_diff;
   Callback.register "decode_tx_pool_diff" decode_tx_pool_diff
